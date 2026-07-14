3e21fd41b1996ee320de09fc378a32941346bb11
Valdeir Sapará <saparavaldeir@gmail.com>
2026-07-13 23:22:17 -0400

    adiciona favicon e atualiza links no template para suporte a ícones

--- _libsql_test_settings.py
+++
@@ -1,9 +0,0 @@
-from config.settings import *  # noqa
-
-# Exercita o backend libSQL com um arquivo local (sem precisar de Turso remoto)
-DATABASES = {
-    'default': {
-        'ENGINE': 'libsql.db.backends.sqlite3',
-        'NAME': 'file:' + str(BASE_DIR / 'scratch' / 'libsql_test.db'),  # noqa: F405
-    }
-}
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/icons/favicon.svg', permanent=True)),
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
     path('', include('reader.urls')),
